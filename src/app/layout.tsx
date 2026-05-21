467ca1fe2e62ee3d4db43b5cb5fb21fd5f55ba38
Anthony <somosvismo@gmail.com>
2026-05-21 13:31:00 -0300

    chore: substitui tags antiga do Google pelo novo container do Google Tag Manager (GTM-NKNHM3BW)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,23 +67,30 @@ export default function RootLayout({
                     async
                     defer
                 />
-                <Script
-                    src="https://www.googletagmanager.com/gtag/js?id=G-2P6C784S1B"
-                    strategy="afterInteractive"
-                />
-                <Script id="google-tracking" strategy="afterInteractive">
+                {/* Google Tag Manager */}
+                <Script id="gtm" strategy="afterInteractive">
                     {`
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
-
-                      gtag('config', 'G-2P6C784S1B');
-                      gtag('config', 'AW-18011790342');
+                      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','GTM-NKNHM3BW');
                     `}
                 </Script>
+                {/* End Google Tag Manager */}
 
             </head>
             <body className={`${dmSans.variable} ${archivo.variable} antialiased bg-[#fff6ef] font-[family-name:var(--font-dm-sans)]`}>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe 
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-NKNHM3BW"
+                        height="0" 
+                        width="0" 
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
 
                 {children}
                 <Script
